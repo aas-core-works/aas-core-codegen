40a418009d7b03e32ce31819e59d85bcb783bc6c
Marko Ristin <marko.ristin@gmail.com>
2021-10-01 18:03:23 +0200

    written initial error container

---
+++ test_data/test_main/scratchy_initial_meta_model_for_v3/input/snippets/Verification/Errors.cs
@@ -0,0 +1,49 @@
+/// <summary>
+/// Contain multiple errors observed during a verification.
+/// </summary>
+public class Errors
+{
+    /// <summary>
+    /// The maximum capacity of the container
+    /// </summary>
+    public readonly int Capacity;
+
+    /// <summary>
+    /// Contained error items
+    /// </summary>
+    public readonly List<Error> Errors;
+
+    /// <summary>
+    /// Initialize the container with the given <paramref name="capacity" />.
+    /// </summary>
+    public Errors(int capacity)
+    {
+        if (capacity <= 0)
+        {
+            throw new System.ArgumentException(
+                $"Expected a strictly positivie capacity, but got: {capacity}");
+        }
+
+        Capacity = capacity;
+        Errors = new List<Error>(Capacity);
+    }
+
+    /// <summary>
+    /// Add the error to the container if the capacity has not been reached.
+    /// </summary>
+    public void Add(Error error)
+    {
+        if(Errors.Count <= Capacity)
+        {
+            Errors.Add(error);
+        }
+    }
+
+    /// <summary>
+    /// True if the capacity has been reached.
+    /// </summary>
+    public boolean Full()
+    {
+        return Errors.Count == Capacity;
+    }
+}
