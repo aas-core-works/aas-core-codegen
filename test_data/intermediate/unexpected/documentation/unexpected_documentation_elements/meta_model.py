1fd2b7c4d101d1b1ddb8e2f3443d0f4d81d4f2d6
Marko Ristin <marko.ristin@gmail.com>
2022-04-08 16:28:37 +0200

    Render the descriptions with smoke at intermediate (#142)
    We introduce an additional verification at the intermediate stage to
    check that all documentation elements can be rendered using a dummy
    smoke renderer. This is necessary since we had bad suprises over at
    aas-core-meta, so we want to catch these issues as soon as possible.

---
+++ test_data/intermediate/unexpected/documentation/unexpected_documentation_elements/meta_model.py
@@ -0,0 +1,34 @@
+"""
+Do something.
+
+    0: An unexpected block quote.
+"""
+
+
+class Something:
+    """
+    Represent something.
+
+        1: An unexpected block quote.
+    """
+
+    something: str
+    """
+    Capture something.
+    
+        2: An unexpected block quote.
+    """
+
+    def __init__(self, something: str) -> None:
+        self.something = something
+
+    def do_something(self) -> None:
+        """
+        Do something.
+
+            3: An unexpected block quote.
+        """
+
+
+__book_url__ = "dummy"
+__book_version__ = "dummy"
