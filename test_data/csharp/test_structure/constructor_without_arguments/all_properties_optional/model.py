f6746222c07eebceb99b058a945f8b7cbe0c5a99
Marko Ristin <marko.ristin@gmail.com>
2022-06-19 10:01:52 +0200

    Allow classes without constructor arguments in C# (#189)
    We originally designed the data structures in C# to allow for empty
    constructors, but eventually moved away from that model. This patch
    allows now constructors with empty arguments, as we do not generate
    empty constructors any more.

---
+++ test_data/csharp/test_structure/constructor_without_arguments/all_properties_optional/model.py
@@ -0,0 +1,9 @@
+class Something:
+    some_property: Optional[str]
+
+    def __init__(self, some_property: Optional[str] = None) -> None:
+        self.some_property = some_property
+
+
+__book_url__ = "dummy"
+__book_version__ = "dummy"
---
+++ test_data/csharp/test_structure/constructor_without_arguments/no_properties/model.py
@@ -0,0 +1,6 @@
+class Something:
+    pass
+
+
+__book_url__ = "dummy"
+__book_version__ = "dummy"
