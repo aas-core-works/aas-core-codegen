afec2cf363b6cb69816e7724a2b58626e2165869
Marko Ristin <marko.ristin@gmail.com>
2022-06-19 10:15:14 +0200

    Make C# classes with children implement interfaces (#190)
    This patch fixes the bug that classes with concrete descendants did not
    implement the interface used to designate both the parent and the child
    class.

---
+++ test_data/csharp/test_structure/concrete_class_with_descendants/model.py
@@ -0,0 +1,15 @@
+# NOTE (mristin, 2022-06-19):
+# We originally wrote this test to check that the concrete class with descendants
+# also implements the interface.
+
+
+class Parent:
+    pass
+
+
+class Child(Parent):
+    pass
+
+
+__book_url__ = "dummy"
+__book_version__ = "dummy"
