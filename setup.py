1695e87af0e15ef377af345380ecda41e423a1a2
Marko Ristin <marko.ristin@gmail.com>
2022-09-02 19:09:17 +0200

    Update test data to aas-core-meta 931b355
    We update the development requirements to and re-record the test data
    for [aas-core-meta 931b355].
    
    [aas-core-meta 931b355]: https://github.com/aas-core-works/aas-core-meta/commit/931b355

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
             "twine",
             "jsonschema==3.2.0",
             "xmlschema==1.10.0",
-            "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@95055a5#egg=aas-core-meta",
+            "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@931b355#egg=aas-core-meta",
         ]
     },
     # fmt: on
