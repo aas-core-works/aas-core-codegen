551624b168723ec427da9848f13657a31e907412
Marko Ristin <marko.ristin@gmail.com>
2022-10-30 13:57:46 +0100

    Update test data to aas-core-meta b2d1230
    We update the development requirements to and re-record the test data
    for [aas-core-meta b2d1230].
    
    [aas-core-meta b2d1230]: https://github.com/aas-core-works/aas-core-meta/commit/b2d1230

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
             "twine",
             "jsonschema==3.2.0",
             "xmlschema==1.10.0",
-            "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@3ffe727#egg=aas-core-meta",
+            "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@b2d1230#egg=aas-core-meta",
         ]
     },
     # fmt: on
