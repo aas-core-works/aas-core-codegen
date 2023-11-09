7f2ae5bd15515e87cf04588e50b7f068d0e740ff
Tobias Langer <tobias.langer@conplement.de>
2023-11-09 15:56:28 +0100

    Add interface generation for Java

--- aas_core_codegen/java/structure/_generate.py
+++ aas_core_codegen/java/structure/_generate.py
@@ -1,5 +1,6 @@
 """Generate the Java data structures from the intermediate representation."""
 import io
+import textwrap
 from typing import (
     cast,
     List,
@@ -12,11 +13,15 @@ from icontract import ensure, require
 from aas_core_codegen import intermediate
 from aas_core_codegen import specific_implementations
 from aas_core_codegen.common import (
+    assert_never,
     Error,
+    Identifier,
     Stripped,
+    indent_but_first_line,
 )
 from aas_core_codegen.java import (
     common as java_common,
+    naming as java_naming,
 )
 from aas_core_codegen.java.common import (
     INDENT as I,
@@ -48,6 +53,126 @@ def verify(
 # endregion
 
 # region Generation
+
+
+@ensure(lambda result: (result[0] is None) ^ (result[1] is None))
+def _generate_interface(
+    cls: intermediate.ClassUnion,
+) -> Tuple[Optional[Stripped], Optional[Error]]:
+    """Generate C# interface for the given class ``cls``."""
+    writer = io.StringIO()
+
+    name = java_naming.interface_name(cls.name)
+
+    inheritances = [inheritance.name for inheritance in cls.inheritances]
+    if len(inheritances) == 0:
+        inheritances = [Identifier("Class")]
+
+    inheritance_names = list(map(java_naming.interface_name, inheritances))
+
+    assert len(inheritances) > 0
+    if len(inheritances) == 1:
+        writer.write(f"public interface {name} extends {inheritance_names[0]}\n{{\n")
+    else:
+        writer.write(f"public interface {name} extends\n")
+        for i, inheritance_name in enumerate(inheritance_names):
+            if i > 0:
+                writer.write(",\n")
+
+            writer.write(textwrap.indent(inheritance_name, II))
+
+        writer.write("\n{\n")
+
+    # Code blocks separated by double newlines and indented once
+    blocks = []  # type: List[Stripped]
+
+    # region Getters and setters
+
+    for prop in cls.properties:
+        if prop.specified_for is not cls:
+            continue
+
+        prop_type = java_common.generate_type(type_annotation=prop.type_annotation)
+        getter_name = java_naming.getter_name(prop.name)
+        setter_name = java_naming.setter_name(prop.name)
+
+        blocks.append(Stripped(f"public {prop_type} {getter_name} ();"))
+        blocks.append(Stripped(f"public {prop_type} {setter_name} ();"))
+
+    # endregion
+
+    # region Signatures
+
+    for method in cls.methods:
+        if method.specified_for is not cls:
+            continue
+
+        signature_blocks = []  # type: List[Stripped]
+
+
+        # fmt: off
+        returns = (
+            java_common.generate_type(type_annotation=method.returns)
+            if method.returns is not None else "void"
+        )
+        # fmt: on
+
+        arg_codes = []  # type: List[Stripped]
+        for arg in method.arguments:
+            arg_type = java_common.generate_type(type_annotation=arg.type_annotation)
+            arg_name = java_naming.argument_name(arg.name)
+            arg_codes.append(Stripped(f"{arg_type} {arg_name}"))
+
+        signature_name = java_naming.method_name(method.name)
+        if len(arg_codes) > 2:
+            arg_block = ",\n".join(arg_codes)
+            arg_block_indented = textwrap.indent(arg_block, I)
+            signature_blocks.append(
+                Stripped(f"public {returns} {signature_name}(\n{arg_block_indented});")
+            )
+        elif len(arg_codes) == 1:
+            signature_blocks.append(
+                Stripped(f"public {returns} {signature_name}({arg_codes[0]});")
+            )
+        else:
+            assert len(arg_codes) == 0
+            signature_blocks.append(Stripped(f"public {returns} {signature_name}();"))
+
+        blocks.append(Stripped("\n".join(signature_blocks)))
+
+    for prop in cls.properties:
+        if prop.specified_for is not cls:
+            continue
+
+        if isinstance(
+            prop.type_annotation, intermediate.OptionalTypeAnnotation
+        ) and isinstance(prop.type_annotation.value, intermediate.ListTypeAnnotation):
+            prop_name = java_naming.property_name(prop.name)
+            items_type = java_common.generate_type(prop.type_annotation.value.items)
+            blocks.append(
+                Stripped(
+                    f"""\
+/**
+ * Iterate over {prop_name}, if set, and otherwise return an empty enumerable.
+ */
+public Iterable<{items_type}> over{prop_name}OrEmpty();"""
+                )
+            )
+
+    # endregion
+
+    if len(blocks) == 0:
+        blocks = [Stripped("// Intentionally empty.")]
+
+    for i, code in enumerate(blocks):
+        if i > 0:
+            writer.write("\n\n")
+
+        writer.write(textwrap.indent(code, I))
+
+    writer.write("\n}")
+
+    return Stripped(writer.getvalue()), None
 
 
 # fmt: off
@@ -120,14 +245,46 @@ public interface IClass
 
     errors = []  # type: List[Error]
 
+    for our_type in symbol_table.our_types:
+        if not isinstance(
+            our_type,
+            (
+                intermediate.AbstractClass,
+            ),
+        ):
+            continue
+
+        if isinstance(
+            our_type, intermediate.AbstractClass
+        ):
+            code, error = _generate_interface(cls=our_type)
+            if error is not None:
+                errors.append(
+                    Error(our_type.parsed.node,
+                          f"Failed to generate the interface code for "
+                          f"the class {our_type.name!r}",
+                          [error],
+                    )
+                )
+                continue
+
+            assert code is not None
+            code_blocks.append(code)
+        else:
+            assert_never(our_type)
+
     if len(errors) > 0:
         return None, errors
+
+    code_blocks_joined = "\n\n".join(code_blocks)
 
     blocks = [
         java_common.WARNING,
         Stripped(
             f"""\
 package {package};
+
+{code_blocks_joined}
 // package {package}"""
         ),
         java_common.WARNING,
