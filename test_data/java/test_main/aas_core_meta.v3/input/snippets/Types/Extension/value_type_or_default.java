bed142885bbe367afb44b68c898ff015075c11ac
mboehm <markus.boehm@conplement.de>
2023-11-23 10:56:01 +0100

    Add kind_or_default.java for Qualifier + refactor javadoc

--- test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Extension/value_type_or_default.java
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Extension/value_type_or_default.java
@@ -1,5 +1,5 @@
   /**
-   * @return the value type or the default value.
+   *  @return the value type {@link DataTypeDefXsd} or the default value if it has not been set.
    */
   public DataTypeDefXsd valueTypeOrDefault(){
     return valueType != null ? valueType : DataTypeDefXsd.STRING;
