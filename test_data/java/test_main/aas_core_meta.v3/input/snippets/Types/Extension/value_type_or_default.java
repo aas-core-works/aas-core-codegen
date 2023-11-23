ea9fb7cff5c6a0e1415b77c654f20aff0c69ec36
mboehm <markus.boehm@conplement.de>
2023-11-23 11:47:29 +0100

    reformat java snippets for types

--- test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Extension/value_type_or_default.java
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Extension/value_type_or_default.java
@@ -1,6 +1,6 @@
-  /**
-   *  @return the value type {@link DataTypeDefXsd} or the default value if it has not been set.
-   */
-  public DataTypeDefXsd valueTypeOrDefault(){
-    return valueType != null ? valueType : DataTypeDefXsd.STRING;
-  }
+/**
+ *  @return the value type {@link DataTypeDefXsd} or the default value if it has not been set.
+ */
+public DataTypeDefXsd valueTypeOrDefault(){
+  return valueType != null ? valueType : DataTypeDefXsd.STRING;
+}
--- test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Qualifier/kind_or_default.java
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Qualifier/kind_or_default.java
@@ -1,6 +1,6 @@
-  /**
-   * @return the {@link QualifierKind} or the default value if it has not been set.
-   */
-  public QualifierKind kindOrDefault(){
-    return kind != null ? kind : QualifierKind.CONCEPT_QUALIFIER;
-  }
+/**
+ * @return the {@link QualifierKind} or the default value if it has not been set.
+ */
+public QualifierKind kindOrDefault(){
+  return kind != null ? kind : QualifierKind.CONCEPT_QUALIFIER;
+}
