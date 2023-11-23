b3a07fdb3d8b9fbd17b2df492a83660cbf95448f
mboehm <markus.boehm@conplement.de>
2023-11-23 11:41:56 +0100

    Add order_relevant_or_default.java_or_default.java for submodel element list

---
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Submodel_element_list/order_relevant_or_default.java
@@ -0,0 +1,6 @@
+  /**
+   * @return if the order of the {@link SubmodelElementList} or the default value if it has not been set.
+   */
+  public Boolean orderRelevantOrDefault(){
+    return orderRelevant != null ? orderRelevant : true;
+  }
