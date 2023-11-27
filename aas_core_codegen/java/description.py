e48cd20098f16af4405bd794602b9dfac5b1602c
Tobias Langer <tobias.langer@conplement.de>
2023-11-27 10:53:09 +0100

    Enforce newline before <p> tags in javadoc strings

--- aas_core_codegen/java/description.py
+++ aas_core_codegen/java/description.py
@@ -648,6 +648,16 @@ class _TextBlock(DBC):
         return f"{self.__class__.__name__}({self.parts!r})"
 
 
+class _EnforceNewParagraph(DBC):
+    """
+    Enforce that the following text starts in a new paragraph.
+    """
+
+    def __repr__(self) -> str:
+        """Generate text representation for easier debugging."""
+        return f"{self.__class__.__name__}()"
+
+
 class _EnforceNewLine(DBC):
     """
     Enforce that the following text starts on a new line.
@@ -660,7 +670,7 @@ class _EnforceNewLine(DBC):
         return f"{self.__class__.__name__}()"
 
 
-_TextDirective = Union[_RelativeIndention, _TextBlock, _EnforceNewLine]
+_TextDirective = Union[_RelativeIndention, _TextBlock, _EnforceNewLine, _EnforceNewParagraph]
 
 
 class _ToTextDirectivesVisitor(_NodeVisitor):
@@ -726,7 +736,7 @@ class _ToTextDirectivesVisitor(_NodeVisitor):
                 len(node.attrs) == 0
             ), f"Unexpected attributes in a node {node.name!r}"
 
-            self.directives.append(_EnforceNewLine())
+            self.directives.append(_EnforceNewParagraph())
 
             # single tag nodes
             self.directives.append(_TextBlock(parts=[f"<p>"]))
@@ -738,6 +748,8 @@ class _ToTextDirectivesVisitor(_NodeVisitor):
 
         elif node.name in ("remarks",):
             # single tag nodes
+            self.directives.append(_EnforceNewParagraph())
+
             self.directives.append(_TextBlock(parts=[f"<p>"]))
 
             for item in node.children.items:
@@ -830,7 +842,7 @@ _TextDirectiveExceptEnforceNewLine = Union[_RelativeIndention, _TextBlock]
 assert_union_without_excluded(
     original_union=_TextDirective,
     subset_union=_TextDirectiveExceptEnforceNewLine,
-    excluded=[_EnforceNewLine],
+    excluded=[_EnforceNewLine, _EnforceNewParagraph],
 )
 
 T = TypeVar("T")
@@ -893,6 +905,17 @@ def _compress_text_directives(
 
                 if not previous_text_block.parts[-1].endswith("\n"):
                     previous_text_block.parts.append("\n")
+        elif isinstance(directive, _EnforceNewParagraph):
+            if previous_text_block is not None:
+                assert len(previous_text_block.parts) > 0
+
+                if previous_text_block.parts[-1].endswith("\n\n"):
+                    pass
+                elif previous_text_block.parts[-1].endswith("\n"):
+                    previous_text_block.parts.append("\n")
+                else:
+                    previous_text_block.parts.append("\n\n")
+
         elif isinstance(directive, _TextBlock):
             assert len(directive.parts) > 0
 
