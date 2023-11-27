b97a55d76658ef2b057b7ac0d89464ed2f6396c3
Tobias Langer <tobias.langer@conplement.de>
2023-11-27 09:05:42 +0100

    Fix code style issue in Java snippets

--- test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Has_kind/kind_or_default.java
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Types/Has_kind/kind_or_default.java
@@ -1,6 +1,6 @@
 /**
  * @return the {@link ModellingKind} or the default value if it has not been set.
  */
-public ModellingKind kindOrDefault(){
+public ModellingKind kindOrDefault() {
   return kind != null ? kind : ModellingKind.INSTANCE;
 }
