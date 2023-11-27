b2206ef0e2b38f17866799053a7fb637abafd0e9
Tobias Langer <tobias.langer@conplement.de>
2023-11-27 08:41:10 +0100

    Fix code style for class generation in Java

--- aas_core_codegen/java/structure/_generate.py
+++ aas_core_codegen/java/structure/_generate.py
@@ -592,8 +592,7 @@ def _generate_descend_method(cls: intermediate.ConcreteClass) -> Stripped:
 /**
  * Iterate recursively over all the class instances referenced from this instance.
  */
-public Iterable<IClass> descend()
-{{
+public Iterable<IClass> descend() {{
 {I}return new {iterable_name}();
 }}"""
     )
@@ -609,8 +608,7 @@ def _generate_descend_once_method(cls: intermediate.ConcreteClass) -> Stripped:
 /**
  * Iterate over all the class instances referenced from this instance.
  */
-public Iterable<IClass> descendOnce()
-{{
+public Iterable<IClass> descendOnce() {{
 {I}return new {iterable_name}();
 }}"""
     )
@@ -1131,13 +1129,13 @@ def _generate_constructor(
         arg_codes.append(Stripped(f"{arg_type} {arg_name}"))
 
     if len(arg_codes) == 0:
-        blocks.append(f"public {cls_name}()\n{{")
+        blocks.append(f"public {cls_name}() {{")
     if len(arg_codes) == 1:
-        blocks.append(f"public {cls_name}({arg_codes[0]})\n{{")
+        blocks.append(f"public {cls_name}({arg_codes[0]}) {{")
     else:
         arg_block = ",\n".join(arg_codes)
         arg_block_indented = textwrap.indent(arg_block, I)
-        blocks.append(Stripped(f"public {cls_name}(\n{arg_block_indented})\n{{"))
+        blocks.append(Stripped(f"public {cls_name}(\n{arg_block_indented}) {{"))
 
     body = []  # type: List[str]
 
@@ -1373,8 +1371,7 @@ public void {setter_name}({arg_type} {prop_name}) {{
 /**
  * Iterate over {{@link {cls_name}#{prop_name}}}, if set, and otherwise return an empty iterator.
  */
-public Iterable<{items_type}> {method_name}()
-{{
+public Iterable<{items_type}> {method_name}() {{
 {I}return {getter_name}().orElseGet(Collections::emptyList);
 }}"""
                 )
@@ -1424,8 +1421,7 @@ public Iterable<{items_type}> {method_name}()
  * Accept the {{@code visitor}} to visit this instance for double dispatch.
  **/
 @Override
-public void accept(IVisitor visitor)
-{{
+public void accept(IVisitor visitor) {{
 {I}visitor.{visit_name}(this);
 }}"""
         )
@@ -1439,10 +1435,9 @@ public void accept(IVisitor visitor)
  * with the {{@code context}}.
  **/
 @Override
-public <TContext> void accept(
+public <ContextT> void accept(
 {I}IVisitorWithContext<TContext> visitor,
-{I}TContext context)
-{{
+{I}ContextT context) {{
 {I}visitor.{visit_name}(this, context);
 }}"""
         )
@@ -1457,8 +1452,7 @@ public <TContext> void accept(
  * Accept the {{@code transformer}} to visit this instance for double dispatch.
  **/
 @Override
-public <T> T transform(ITransformer<T> transformer)
-{{
+public <T> T transform(ITransformer<T> transformer) {{
 {I}return transformer.{transform_name}(this);
 }}"""
         )
@@ -1473,7 +1467,7 @@ public <T> T transform(ITransformer<T> transformer)
  **/
 @Override
 public <TContext, T> T transform(
-{I}ITransformerWithContext<TContext, T> transformer,
+{I}ITransformerWithContext<ContextT, T> transformer,
 {I}TContext context)
 {{
 {I}return transformer.{transform_name}(this, context);
@@ -1522,7 +1516,7 @@ public <TContext, T> T transform(
         writer.write(comment)
         writer.write("\n")
 
-    writer.write(f"public class {name} implements {interface_name}\n{{\n")
+    writer.write(f"public class {name} implements {interface_name} {{\n")
 
     for i, block in enumerate(blocks):
         if i > 0:
