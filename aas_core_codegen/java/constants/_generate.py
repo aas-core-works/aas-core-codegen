a85bdd587071097660061912d4517a70063c30f4
Tobias Langer <tobias.langer@conplement.de>
2023-11-28 07:38:42 +0100

    Add basic file generation for Java constants

--- aas_core_codegen/java/constants/_generate.py
+++ aas_core_codegen/java/constants/_generate.py
@@ -1,4 +1,6 @@
 """Generate the Java constants corresponding to the constants of the meta-model."""
+import io
+import textwrap
 from typing import (
     List,
     Optional,
@@ -10,9 +12,13 @@ from icontract import ensure
 from aas_core_codegen import intermediate
 from aas_core_codegen.common import (
     Error,
+    Stripped,
 )
 from aas_core_codegen.java import (
     common as java_common,
+)
+from aas_core_codegen.csharp.common import (
+    INDENT as I,
 )
 
 # region Generation
@@ -30,7 +36,54 @@ def generate(
     symbol_table: intermediate.SymbolTable,
     package: java_common.PackageIdentifier,
 ) -> Tuple[Optional[str], Optional[List[Error]]]:
-    raise NotImplementedError
+    """
+    Generate the Java code of the constants based on the symbol table.
+
+    The ``package`` defines the AAS Java package.
+    """
+    constants_blocks = []  # type: List[Stripped]
+
+    constants_writer = io.StringIO()
+    constants_writer.write(
+        """\
+/**
+ * Provide constant values of the meta-model.
+ */
+public class Constants {
+"""
+    )
+
+    for i, constants_block in enumerate(constants_blocks):
+        assert len(constants_block) > 0
+
+        if i > 0:
+            constants_writer.write("\n\n")
+
+        constants_writer.write(textwrap.indent(constants_block, I))
+
+    constants_writer.write("\n}")
+
+    blocks = [
+        java_common.WARNING,
+        Stripped(
+            f"""\
+package {package}.constants;
+
+{constants_writer.getvalue()}"""
+        ),
+        java_common.WARNING,
+    ]  # type: List[Stripped]
+
+    out = io.StringIO()
+    for i, constants_block in enumerate(blocks):
+        if i > 0:
+            out.write("\n\n")
+
+        out.write(constants_block)
+
+    out.write("\n")
+
+    return out.getvalue(), None
 
 
 # endregion
