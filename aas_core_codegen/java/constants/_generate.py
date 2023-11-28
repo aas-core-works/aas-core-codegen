6cbd9f726bbcc51e84e72835ad07d268dac2aea3
Tobias Langer <tobias.langer@conplement.de>
2023-11-28 08:55:15 +0100

    Add support for ConstantPrimitive generation in Java

--- aas_core_codegen/java/constants/_generate.py
+++ aas_core_codegen/java/constants/_generate.py
@@ -11,17 +11,77 @@ from icontract import ensure
 
 from aas_core_codegen import intermediate
 from aas_core_codegen.common import (
+    assert_never,
     Error,
     Stripped,
 )
 from aas_core_codegen.java import (
     common as java_common,
+    naming as java_naming,
 )
 from aas_core_codegen.csharp.common import (
     INDENT as I,
 )
 
 # region Generation
+
+
+def _byte_array_as_expr(value: bytearray) -> Stripped:
+    """Return a multi-line java code representing the ``value''."""
+    writer = io.StringIO()
+
+    writer.write("new byte[] {")
+
+    for i, byte in enumerate(value):
+        if i > 0:
+            if i % 8 == 0:
+                writer.write(",\n")
+            else:
+                writer.write(", ")
+
+        writer.write(f"(byte) 0x{byte:02x}")
+
+    writer.write("\n}")
+
+    return Stripped(writer.getvalue())
+
+
+@ensure(lambda result: (result[0] is None) ^ (result[1] is None))
+def _generate_constant_primitive(
+        constant: intermediate.ConstantPrimitive,
+) -> Tuple[Optional[Stripped], Optional[Error]]:
+    """Generate the definition of a constant primitive."""
+    constant_name = java_naming.property_name(constant.name)
+
+    assert constant.a_type in java_common.PRIMITIVE_TYPE_MAP
+
+    java_type = java_common.PRIMITIVE_TYPE_MAP[constant.a_type]
+
+    literal = None  # type: Optional[Stripped]
+
+    if constant.a_type is intermediate.PrimitiveType.BOOL:
+        literal = Stripped("true") if constant.value else Stripped("false")
+
+    elif constant.a_type is intermediate.PrimitiveType.INT:
+        literal = Stripped(str(constant.value))
+
+    elif constant.a_type is intermediate.PrimitiveType.FLOAT:
+        literal = Stripped(str(constant.value))
+
+    elif constant.a_type is intermediate.PrimitiveType.STR:
+        assert isinstance(constant.value, str)
+        literal = java_common.string_literal(constant.value)
+
+    elif constant.a_type is intermediate.PrimitiveType.BYTEARRAY:
+        assert isinstance(constant.value, bytearray)
+        literal = _byte_array_as_expr(value=constant.value)
+
+    else:
+        assert_never(constant.a_type)
+
+    assert literal is not None
+
+    return Stripped(f"public static const {java_type} {constant_name} = {literal};"), None
 
 
 # fmt: off
@@ -42,6 +102,33 @@ def generate(
     The ``package`` defines the AAS Java package.
     """
     constants_blocks = []  # type: List[Stripped]
+
+    errors = []  # type: List[Error]
+
+    for constant in symbol_table.constants:
+        constants_block = None  # type: Optional[Stripped]
+        error = None  # type: Optional[Error]
+
+        if isinstance(constant, intermediate.ConstantPrimitive):
+            constants_block, error = _generate_constant_primitive(constant)
+        elif isinstance(constant, intermediate.ConstantSetOfPrimitives):
+            print("TODO: Constant generation for ConstantSetOfPrimitives types not implemented yet.")
+            continue
+        elif isinstance(constant, intermediate.ConstantSetOfEnumerationLiterals):
+            print("TODO: Constant generation for ConstantSetOfEnumerationLiterals types not implemented yet.")
+            continue
+        else:
+            assert_never(constant)
+
+        if error is not None:
+            errors.append(error)
+            continue
+
+        assert constants_block is not None
+        constants_blocks.append(constants_block)
+
+    if len(errors) > 0:
+        return None, errors
 
     constants_writer = io.StringIO()
     constants_writer.write(
