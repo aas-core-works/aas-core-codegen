97fb5556867731f555c166f11986880a7de6d218
mboehm <markus.boehm@conplement.de>
2023-12-20 17:12:04 +0100

    Add reference_key_values_equal.java

---
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Verification/reference_key_values_equal.java
@@ -0,0 +1,25 @@
+/**
+* Check that the two references,'that' and
+* 'other', are equal by comparing
+* their {@link IReference#getKeys() keys} by
+* {@link IKey#getValue() value}'s.
+*/
+public static boolean referenceKeyValuesEqual(
+        IReference that,
+        IReference other
+){
+    if (that.getKeys().size() != other.getKeys().size())
+    {
+        return false;
+    }
+
+    for (int i = 0; i < that.getKeys().size(); i++)
+    {
+        if (!Objects.equals(that.getKeys().get(i).getValue(), other.getKeys().get(i).getValue()))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
