ae61d0abbb223f08c28a44cb0812f4e56a2961c9
mboehm <markus.boehm@conplement.de>
2023-12-20 15:07:45 +0100

    Add data_specification_IEC_61360s_have_definition_at_least_in_english.java

---
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Verification/data_specification_IEC_61360s_have_definition_at_least_in_english.java
@@ -0,0 +1,34 @@
+/**
+ * Check that the {@link IDataSpecificationIec61360#getDefinition() definition} is defined
+* for all data specifications whose content is given as IEC 61360 at least in English.
+*/
+public static boolean dataSpecificationIec61360sHaveDefinitionAtLeastInEnglish(
+        Iterable<IEmbeddedDataSpecification> embeddedDataSpecifications
+){
+    for (IEmbeddedDataSpecification embeddedDataSpecification : embeddedDataSpecifications){
+        IDataSpecificationIec61360 iec61360 = (IDataSpecificationIec61360) embeddedDataSpecification.getDataSpecificationContent();
+        if (iec61360 != null)
+        {
+            if (!iec61360.getDefinition().isPresent())
+            {
+                return false;
+            }
+
+            boolean noDefinitionInEnglish = true;
+            for (ILangStringDefinitionTypeIec61360 langString : iec61360.getDefinition().get())
+            {
+                if (isBcp47ForEnglish(langString.getLanguage()))
+                {
+                    noDefinitionInEnglish = false;
+                    break;
+                }
+            }
+
+            if (noDefinitionInEnglish)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
