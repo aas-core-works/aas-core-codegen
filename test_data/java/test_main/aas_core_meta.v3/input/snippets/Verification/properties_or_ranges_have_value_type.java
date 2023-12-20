b26a6dbbaa557ce46cabc8c19cd0aaf4f070e1e8
mboehm <markus.boehm@conplement.de>
2023-12-20 16:55:03 +0100

    Add properties_or_ranges_have_value_type.java

---
+++ test_data/java/test_main/aas_core_meta.v3/input/snippets/Verification/properties_or_ranges_have_value_type.java
@@ -0,0 +1,27 @@
+/**
+* Check that the elements which are
+* {@link IProperty} or {@link IRange}
+* have the given valueType.
+ * @param elements the elements.
+ * @param valueType the valueType.
+*/
+public static boolean propertiesOrRangesHaveValueType(
+        Iterable<ISubmodelElement> elements,
+        DataTypeDefXsd valueType
+){
+    for (ISubmodelElement element : elements)
+    {
+       if(element instanceof IProperty){
+           if (((IProperty) element).getValueType() != valueType)
+           {
+               return false;
+           }
+       } else if (element instanceof IRange) {
+           if (((IRange) element).getValueType() != valueType)
+           {
+               return false;
+           }
+       }
+    }
+    return true;
+}
