85ba2d770a04e05b49cacd450b54a7991ab24f35
Marko Ristin <marko.ristin@gmail.com>
2024-03-22 21:57:18 +0100

    Fix rendering of max. quantifier in regexes
    There are many different syntaxes for regular expressions. Some of them
    require the min. quantifier to be explicitly specified as 0 even if only
    the max. quantifier is given. To comply with such syntaxes, we
    explicitly render the max. quantifier without a min. quantifier as
    `{0,m}`.

--- tests/xsd/test_main.py
+++ tests/xsd/test_main.py
@@ -69,18 +69,36 @@ class Test_undo_escaping_x(unittest.TestCase):
         )
 
 
-class Test_removing_anchors_in_patterns(unittest.TestCase):
+class Test_translate_pattern(unittest.TestCase):
     # NOTE (mristin, 2022-06-18):
     # This is relevant since XSD are always anchored.
     # See: https://stackoverflow.com/questions/4367914/regular-expression-in-xml-schema-definition-fails
 
-    def test_table(self) -> None:
+    def test_table_for_removing_anchors(self) -> None:
         for pattern, expected, identifier in [
             ("^$", "", "empty"),
             ("^something$", "something", "simple_literal"),
             ("(^.*$)", "(.*)", "within_a_group"),
         ]:
-            fixed, error = xsd_main._remove_anchors_in_pattern(pattern)
+            fixed, error = xsd_main._translate_pattern(pattern)
+            assert error is None, identifier
+            assert fixed is not None, identifier
+
+            self.assertEqual(expected, fixed, identifier)
+
+    def test_table_for_rendering_quantifiers(self) -> None:
+        # NOTE (mristin, 2024-03-22):
+        # We explicitly test for quantifiers to make sure that they all comply with
+        # XSD patterns. For example, when only the maximum quantifier is given,
+        # the minimum quantifier of 0 must be indicated explicitly.
+        for pattern, expected, identifier in [
+            ("a{1}", "a{1}", "exact repetition"),
+            ("a{1,}", "a+", "min 1 repetition"),
+            ("a{2,}", "a{2,}", "more than 1 min repetition"),
+            ("a{,2}", "a{0,2}", "only max repetition"),
+            ("a{1,2}", "a{1,2}", "min and max repetition"),
+        ]:
+            fixed, error = xsd_main._translate_pattern(pattern)
             assert error is None, identifier
             assert fixed is not None, identifier
 
