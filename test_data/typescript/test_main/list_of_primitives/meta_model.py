cd317a4dc2e040527e36f9b9111f0bb8efcc4f21
Björn Otto <bjoern.otto@ifak.eu>
2025-02-20 14:30:13 +0100

    Support typescript

---
+++ test_data/typescript/test_main/list_of_primitives/meta_model.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Foo:
+    pass
+
+
+class List_of_primitives:
+    strings: List[str]
+    integers: List[int]
+    booleans: List[bool]
+
+    def __init__(
+        self, strings: List[str], integers: List[int], booleans: List[bool]
+    ) -> None:
+        self.strings = strings
+        self.integers = integers
+        self.booleans = booleans
+
+
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
