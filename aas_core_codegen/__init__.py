fb829aed7bf02b68010d1f464099c2b06b1ec46a
Marko Ristin-Kaufmann <marko@ristin.ch>
2025-12-17 23:49:46 +0100

    Fix CI
    We forgot to synchronize the version in `__init__.py` with the
    `pyproject.toml`.

--- aas_core_codegen/__init__.py
+++ aas_core_codegen/__init__.py
@@ -1,6 +1,6 @@
 """Generate implementations and schemas based on an AAS meta-model."""
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 __author__ = "Marko Ristin, Nico Braunisch"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Production/Stable"
