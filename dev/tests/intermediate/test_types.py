0dceee8c81b8c73c2a3bd0ad7d44a308e63f7bb1
Marko Ristin <marko@ristin.ch>
2025-12-24 15:43:05 +0100

    Extend `must_find` to constants (#564)
    We extend the methods `intermediate.SymbolTable.must_find_*` for
    constants so that the downstream code can use them for more readable
    transformations.
    
    For example, we currently use these must-find constant methods in
    aas-core3.1-testdatagen.

--- dev/tests/intermediate/test_types.py
+++ dev/tests/intermediate/test_types.py
@@ -4,6 +4,7 @@ import textwrap
 import unittest
 
 import tests.common
+from aas_core_codegen import intermediate
 from aas_core_codegen.common import Identifier
 
 
@@ -163,6 +164,126 @@ class TestIsSubclassOf(unittest.TestCase):
         self.assertTrue(concrete.is_subclass_of(cls=concrete))
         self.assertTrue(concrete.is_subclass_of(cls=parent))
         self.assertFalse(concrete.is_subclass_of(cls=another_concrete))
+
+
+class TestMustFindConstant(unittest.TestCase):
+    def test_empty(self) -> None:
+        source = """\
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
+"""
+        symbol_table, error = tests.common.translate_source_to_intermediate(
+            source=source
+        )
+        assert error is None, tests.common.most_underlying_messages(error)
+        assert symbol_table is not None
+
+        with self.assertRaises(KeyError):
+            _ = symbol_table.must_find_constant(Identifier("Something"))
+
+    def test_constant(self) -> None:
+        source = """\
+Something: int = constant_int(value=1984)
+
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
+"""
+        symbol_table, error = tests.common.translate_source_to_intermediate(
+            source=source
+        )
+        assert error is None, tests.common.most_underlying_messages(error)
+        assert symbol_table is not None
+
+        result = symbol_table.must_find_constant(Identifier("Something"))
+        assert isinstance(result, intermediate.Constant)
+
+    def test_constant_primitive(self) -> None:
+        source = """\
+Something: int = constant_int(value=1984)
+
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
+"""
+        symbol_table, error = tests.common.translate_source_to_intermediate(
+            source=source
+        )
+        assert error is None, tests.common.most_underlying_messages(error)
+        assert symbol_table is not None
+
+        result = symbol_table.must_find_constant_primitive(Identifier("Something"))
+        assert isinstance(result, intermediate.ConstantPrimitive)
+
+        with self.assertRaises(TypeError):
+            _ = symbol_table.must_find_constant_set_of_primitives(
+                Identifier("Something")
+            )
+
+        with self.assertRaises(TypeError):
+            _ = symbol_table.must_find_constant_set_of_enumeration_literals(
+                Identifier("Something")
+            )
+
+    def test_constant_set_of_primitives(self) -> None:
+        source = """\
+Something: Set[str] = constant_set(
+    values=["hello", "world"]
+)
+
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
+        """
+        symbol_table, error = tests.common.translate_source_to_intermediate(
+            source=source
+        )
+        assert error is None, tests.common.most_underlying_messages(error)
+        assert symbol_table is not None
+
+        result = symbol_table.must_find_constant_set_of_primitives(
+            Identifier("Something")
+        )
+        assert isinstance(result, intermediate.ConstantSetOfPrimitives)
+
+        with self.assertRaises(TypeError):
+            _ = symbol_table.must_find_constant_primitive(Identifier("Something"))
+
+        with self.assertRaises(TypeError):
+            _ = symbol_table.must_find_constant_set_of_enumeration_literals(
+                Identifier("Something")
+            )
+
+    def test_constant_set_of_enumeration_literals(self) -> None:
+        source = """\
+class SomeEnum(Enum):
+    Some_literal = "SOME-LITERAL"
+    Another_literal = "ANOTHER-LITERAL"
+    Yet_another_literal = "YET-ANOTHER-LITERAL"
+
+
+Something: Set[SomeEnum] = constant_set(
+    values=[SomeEnum.Some_literal, SomeEnum.Another_literal]
+)
+
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
+        """
+        symbol_table, error = tests.common.translate_source_to_intermediate(
+            source=source
+        )
+        assert error is None, tests.common.most_underlying_messages(error)
+        assert symbol_table is not None
+
+        result = symbol_table.must_find_constant_set_of_enumeration_literals(
+            Identifier("Something")
+        )
+        assert isinstance(result, intermediate.ConstantSetOfEnumerationLiterals)
+
+        with self.assertRaises(TypeError):
+            _ = symbol_table.must_find_constant_primitive(Identifier("Something"))
+
+        with self.assertRaises(TypeError):
+            _ = symbol_table.must_find_constant_set_of_primitives(
+                Identifier("Something")
+            )
 
 
 if __name__ == "__main__":
