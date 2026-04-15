aa24525a2798216a48374a7b74dcc37515eab9b0
Marko Ristin-Kaufmann <marko@ristin.ch>
2026-04-15 10:17:14 +0200

    Add live tests for Python
    We add an integration test for Python generator where we check the code
    with mypy and pylint, and run the generated unit tests if there is test
    data available for the test case.

--- dev/tests/test_main.py
+++ dev/tests/test_main.py
@@ -423,6 +423,11 @@ class Test_python(_TestCase):
             target=aas_core_codegen.main.Target.PYTHON, case_name="aas_core_meta.v3"
         )
 
+    def test_expected_primitive_types(self) -> None:
+        self._run_expected_test(
+            target=aas_core_codegen.main.Target.PYTHON, case_name="primitive_types"
+        )
+
 
 class Test_python_protobuf(_TestCase):
     def test_expected_aas_core_meta_v3(self) -> None:
