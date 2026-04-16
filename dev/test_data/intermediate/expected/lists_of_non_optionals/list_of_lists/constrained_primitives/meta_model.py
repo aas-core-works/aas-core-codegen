739b6972ee0fccde85b18bcda6061d869ec4c4fa
Marko Ristin-Kaufmann <marko@ristin.ch>
2026-04-16 10:16:12 +0200

    Support list of non-optionals in intermediate
    We add support for list of non-optionals in the intermediate layer since
    we want to support it in most of generators.

---
+++ dev/test_data/intermediate/expected/lists_of_non_optionals/list_of_lists/constrained_primitives/meta_model.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from icontract import invariant
+
+
+@invariant(lambda self: self > 0, "Larger than zero")
+class Positive(int):
+    pass
+
+
+class Something:
+    list_of_lists_of_positives: List[List[Positive]]
+
+    def __init__(self, list_of_lists_of_positives: List[List[Positive]]) -> None:
+        self.list_of_lists_of_positives = list_of_lists_of_positives
+
+
+__version__ = "dummy"
+__xml_namespace__ = "https://dummy.com"
