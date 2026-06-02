fe0a9ec42de3b939613efc99dae162e96501f2f9
Marko Ristin-Kaufmann <marko@ristin.ch>
2026-06-02 11:20:57 +0200

    Allow for missing value data type in XSD
    We change the XSD generator so that it can handle the case when the
    constrained primitive ``Value_data_type`` is missing. This allows us to
    have a uniform approach to meta-models. For example, we can share the
    meta-models across the tests, and need no special handling of XSD
    anymore.

--- dev/test_data/main/xsd/expected/list_of_classes_with_invariants/meta_model.py
+++ dev/test_data/main/xsd/expected/list_of_classes_with_invariants/meta_model.py
@@ -4,15 +4,6 @@ from typing import List
 from icontract import invariant
 
 
-# NOTE (mristin):
-# The XSD generator expects the constrained primitive Value data type to be defined
-# since we had to hard-wire it. If we remove that hard-wiring, we can also remove
-# this definition as well.
-
-class Value_data_type(str, DBC):
-    pass
-
-
 @abstract
 @serialization(with_model_type=True)
 class Abstract_item:
